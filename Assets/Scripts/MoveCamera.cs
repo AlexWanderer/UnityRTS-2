5b7fa8f409cf2ee12e4da25f9d36b9a29269207c
Alissa Hall <alissa.hall@utexas.edu>
2016-04-15 14:08:34 -0500

    mouse controlled camera movement added

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+	//
+	// VARIABLES
+	//
+	
+	public float panSpeed = 0.1f;		// Speed of the camera when being panned
+	public float zoomSpeed = 4.0f;		// Speed of the camera going back and forth
+	
+	private Vector3 mouseOrigin;	// Position of cursor when mouse dragging starts
+	private bool isPanning;		// Is the camera being panned?
+	private bool isRotating;	// Is the camera being rotated?
+	private bool isZooming;		// Is the camera zooming?
+	
+	//
+	// UPDATE
+	//
+	
+	void Update () 
+	{
+		
+		// Disable movements on button release
+		if (!Input.GetMouseButton(2)) isZooming=false;
+		
+		
+		// Move the camera on it's XY plane
+		if (!isZooming)
+		{
+	        	Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+	        	Vector3 move = new Vector3(0, 0, 0); 
+	        	if (pos.x <= 0.01f || pos.x >= 0.99f || pos.y <= 0.01f || pos.y >= 0.99f){
+		        	move += new Vector3((pos.x-0.5f)*panSpeed, 0, (pos.y-0.5f)*panSpeed);
+		        }
+
+		        transform.Translate(move, Space.World);
+		}
+		
+		// Move the camera linearly along Z axis
+		if (isZooming)
+		{
+	        	Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+	        	Vector3 move = pos.y * zoomSpeed * transform.forward; 
+	        	transform.Translate(move, Space.World);
+		}
+	}
+}
