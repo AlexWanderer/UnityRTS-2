7a648e53052d3dbdb4c35bebc5a97b93ed482270
Anurag Papineni <Anurag Papineni>
2016-04-19 23:34:54 -0500

    Made changes to camera movement and added functionality to get plane position on click

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    // Update is called once per frame
+    public Plane groundPlane;
+    public Transform markerObject;
+    public static float planex, planey;
+
+    void Start()
+    {
+        groundPlane = new Plane(new Vector3(0, 1, 0), new Vector3(0,0,0)); 
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float rayDistance;
+            if (groundPlane.Raycast(ray, out rayDistance))
+            {
+                planex = ray.GetPoint(rayDistance).x;
+                planey = ray.GetPoint(rayDistance).z;
+            }
+        }
+    }
+}
--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -8,10 +8,6 @@ public class MoveCamera : MonoBehaviour {
 	
 	public float panSpeed = 0.1f;		// Speed of the camera when being panned
 	public float zoomSpeed = 4.0f;		// Speed of the camera going back and forth
-	
-	private Vector3 mouseOrigin;	// Position of cursor when mouse dragging starts
-	private bool isPanning;		// Is the camera being panned?
-	private bool isRotating;	// Is the camera being rotated?
 	private bool isZooming;		// Is the camera zooming?
 	
 	//
@@ -24,23 +20,28 @@ public class MoveCamera : MonoBehaviour {
 		// Disable movements on button release
 		if (!Input.GetMouseButton(2)) isZooming=false;
 		
-		
-		// Move the camera on it's XY plane
-		if (!isZooming)
+        // Move the camera on it's XY plane
+        if (!isZooming)
 		{
-	        	Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+	        	Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
 	        	Vector3 move = new Vector3(0, 0, 0); 
-	        	if (pos.x <= 0.01f || pos.x >= 0.99f || pos.y <= 0.01f || pos.y >= 0.99f){
+	        	if (pos.x <= 0.1f || pos.x >= 0.90f || pos.y <= 0.1f || pos.y >= 0.9f){
 		        	move += new Vector3((pos.x-0.5f)*panSpeed, 0, (pos.y-0.5f)*panSpeed);
 		        }
 
 		        transform.Translate(move, Space.World);
-		}
+                Vector3 p = transform.position;
+                if (System.Math.Abs(p.x) > 5.0f)
+                    transform.Translate(new Vector3(-move.x,0,0), Space.World);
+                if (System.Math.Abs(p.z) > 5.0f)
+                    transform.Translate(new Vector3(0, 0, -move.z), Space.World);
+
+        }
 		
 		// Move the camera linearly along Z axis
 		if (isZooming)
 		{
-	        	Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+	        	Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
 
 	        	Vector3 move = pos.y * zoomSpeed * transform.forward; 
 	        	transform.Translate(move, Space.World);
