7a9ebf0c8deaf86c28f42167397071936d04e7eb
Alissa Hall <alissa.hall@utexas.edu>
2016-04-22 14:58:02 -0500

    selection box fix

--- Assets/Player/HUD/HUD.cs
+++ Assets/Player/HUD/HUD.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using RTS;
 
 public class HUD : MonoBehaviour {
 
-	public GUISkin resourceSkin, ordersSkin;
+	public GUISkin resourceSkin, ordersSkin, selectBoxSkin;
 	private const int ORDERS_BAR_WIDTH = 150, RESOURCE_BAR_HEIGHT = 40;
+	private const int SELECTION_NAME_HEIGHT = 15;
 	private Player player;
 
 	// Use this for initialization
 	void Start () {
 		player = transform.root.GetComponent< Player >();
+		ResourceManager.StoreSelectBoxItems(selectBoxSkin);
 	}
 	
 	// Update is called once per frame
@@ -23,8 +26,15 @@ public class HUD : MonoBehaviour {
 
 	private void DrawOrdersBar() {
 	    GUI.skin = ordersSkin;
-	    GUI.BeginGroup(new Rect(Screen.width-ORDERS_BAR_WIDTH,RESOURCE_BAR_HEIGHT,ORDERS_BAR_WIDTH,Screen.height-RESOURCE_BAR_HEIGHT));
+	    GUI.BeginGroup(new Rect(Screen.width - ORDERS_BAR_WIDTH,RESOURCE_BAR_HEIGHT,ORDERS_BAR_WIDTH,Screen.height-RESOURCE_BAR_HEIGHT));
 	    GUI.Box(new Rect(0,0,ORDERS_BAR_WIDTH,Screen.height-RESOURCE_BAR_HEIGHT),"");
+	    string selectionName = "";
+		if(player.SelectedObject) {
+		    selectionName = player.SelectedObject.objectName;
+		}
+		if(!selectionName.Equals("")) {
+		    GUI.Label(new Rect(0,10,ORDERS_BAR_WIDTH,SELECTION_NAME_HEIGHT), selectionName);
+		}
 	    GUI.EndGroup();
 	}
 
@@ -33,5 +43,18 @@ public class HUD : MonoBehaviour {
 	    GUI.BeginGroup(new Rect(0,0,Screen.width,RESOURCE_BAR_HEIGHT));
 	    GUI.Box(new Rect(0,0,Screen.width,RESOURCE_BAR_HEIGHT),"");
 	    GUI.EndGroup();
+	}
+
+	public bool MouseInBounds() {
+	    //Screen coordinates start in the lower-left corner of the screen
+	    //not the top-left of the screen like the drawing coordinates do
+	    Vector3 mousePos = Input.mousePosition;
+	    bool insideWidth = mousePos.x >= 0 && mousePos.x <= Screen.width - ORDERS_BAR_WIDTH;
+	    bool insideHeight = mousePos.y >= 0 && mousePos.y <= Screen.height - RESOURCE_BAR_HEIGHT;
+	    return insideWidth && insideHeight;
+	}
+
+	public Rect GetPlayingArea() {
+	    return new Rect(0, RESOURCE_BAR_HEIGHT, Screen.width - ORDERS_BAR_WIDTH, Screen.height - RESOURCE_BAR_HEIGHT);
 	}
 }
