963b6f421743c16dc0f85fcc2073b555fac3e30b
Anurag Papineni <Anurag Papineni>
2016-04-29 22:58:12 -0500

    Pathfinding works, but crashes for longer paths

--- Assets/WorldObject/Unit/Unit.cs
+++ Assets/WorldObject/Unit/Unit.cs
@@ -5,93 +5,100 @@ using RTS;
 using System;
 
 public class Unit : WorldObject {
-	protected bool moving, rotating;
-	 
-	private Vector3 destination;
+    protected bool moving, rotating;
+
+    private Vector3 destination;
     private Vector3 pathpoint;
-	private Quaternion targetRotation;
-	private GameObject destinationTarget;
+    private Quaternion targetRotation;
+    private GameObject destinationTarget;
     private Stack<Vector3> path = new Stack<Vector3>();
-	public float moveSpeed, rotateSpeed;
- 
- 
+    private Building[] buildings;
+    private bool validPath;
+    public float moveSpeed, rotateSpeed;
+
+
     /*** Game Engine methods, all can be overridden by subclass ***/
- 
+
     protected override void Awake() {
         base.Awake();
     }
- 
-    protected override void Start () {
+
+    protected override void Start() {
         base.Start();
+        buildings = FindObjectsOfType(typeof(Building)) as Building[];
     }
- 
-    protected override void Update () {
-	    base.Update();
-	    if(rotating) TurnToTarget();
-	    else if(moving) MakeMove();
-	}
- 
+
+    protected override void Update() {
+        base.Update();
+        if (rotating) TurnToTarget();
+        else if (moving) MakeMove();
+    }
+
     protected override void OnGUI() {
         base.OnGUI();
     }
 
     public virtual void SetBuilding(Building building) {
-		//specific initialization for a unit can be specified here
-	}
-
-    public override void MouseClick(GameObject hitObject, Vector3 hitPoint, Player controller) {
-	    base.MouseClick(hitObject, hitPoint, controller);
-	    //only handle input if owned by a human player and currently selected
-	    if(player && player.human && currentlySelected) {
-	        if(hitObject.name == "Ground" && hitPoint != ResourceManager.InvalidPosition) {
-                attacking = false;
-	            float x = hitPoint.x;
-	            //makes sure that the unit stays on top of the surface it is on
-	            float y = hitPoint.y + player.SelectedObject.transform.position.y;
-	            float z = hitPoint.z;
-	            Vector3 destination = new Vector3(x, y, z);
-	            StartMove(destination);
-	        }
-	    }
+        //specific initialization for a unit can be specified here
     }
 
-	public virtual void StartMove(Vector3 destination) {
-	    this.destination = destination;
-        idastar();
-        pathpoint = path.Pop();
-	    targetRotation = Quaternion.LookRotation (destination - transform.position);
-	    rotating = true;
-	    moving = false;
-	}
+    public override void MouseClick(GameObject hitObject, Vector3 hitPoint, Player controller) {
+        base.MouseClick(hitObject, hitPoint, controller);
+        //only handle input if owned by a human player and currently selected
+        if (player && player.human && currentlySelected) {
+            if (hitObject.name == "Ground" && hitPoint != ResourceManager.InvalidPosition) {
+                attacking = false;
+                float x = hitPoint.x;
+                //makes sure that the unit stays on top of the surface it is on
+                float y = hitPoint.y + player.SelectedObject.transform.position.y;
+                float z = hitPoint.z;
+                Vector3 destination = new Vector3(x, y, z);
+                StartMove(destination);
+            }
+        }
+    }
 
-	public void StartMove(Vector3 destination, GameObject destinationTarget) {
-		StartMove(destination);
-		this.destinationTarget = destinationTarget;
-	}
+    public virtual void StartMove(Vector3 destination) {
+        this.destination = destination;
+        if ( idastar())
+            pathpoint = path.Pop();
+        targetRotation = Quaternion.LookRotation(destination - transform.position);
+        rotating = true;
+        moving = false;
+    }
 
-	private void TurnToTarget() {
-	    transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed);
+    public void StartMove(Vector3 destination, GameObject destinationTarget) {
+        StartMove(destination);
+        this.destinationTarget = destinationTarget;
+    }
 
-	    CalculateBounds();
-	    //sometimes it gets stuck exactly 180 degrees out in the calculation and does nothing, this check fixes that
-	    Quaternion inverseTargetRotation = new Quaternion(-targetRotation.x, -targetRotation.y, -targetRotation.z, -targetRotation.w);
-	    if(transform.rotation == targetRotation || transform.rotation == inverseTargetRotation) {
-	        rotating = false;
-	        moving = true;
-	    }
-	}
+    private void TurnToTarget() {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed);
+
+        CalculateBounds();
+        //sometimes it gets stuck exactly 180 degrees out in the calculation and does nothing, this check fixes that
+        Quaternion inverseTargetRotation = new Quaternion(-targetRotation.x, -targetRotation.y, -targetRotation.z, -targetRotation.w);
+        if (transform.rotation == targetRotation || transform.rotation == inverseTargetRotation) {
+            rotating = false;
+            moving = true;
+        }
+    }
     public bool idastar()
     {
         path.Clear();
         float bound = Vector3.Distance(transform.position, destination);
         float t = 0;
-        for (int i=0; i<30;i++)
+        while (true)
         {
             t = search(transform.position, 0, bound);
             if (t == -1.0f)
+            {
                 return true;
-            else if ( float.IsPositiveInfinity(t))
+            }
+            else if (float.IsPositiveInfinity(t))
+            { 
                 return false;
+            }
             bound = t;
         }
         return false;
@@ -102,19 +109,18 @@ public class Unit : WorldObject {
         float f = g + Vector3.Distance(node, destination);
         if (f > bound)
             return f;
-        if (isGoal(node)){
+        if (isGoal(node)) {
             path.Push(node);
             return -1;
         }
         float min = float.PositiveInfinity;
         float t;
-        Vector3 successor;
-        for (float i = 0; i<360; i+= 20)
+        List<Vector3> successors = getSuccessors(node);
+        foreach (Vector3 successor in successors)
         {
-            successor = getNextbyAngle(node, i);
-            if (isValidPosition(successor)){
-                t = search(successor, g + .1f, bound);
-                if (t == -1){
+            if (isValidPosition(successor)) {
+                t = search(successor, g + 3.0f, bound);
+                if (t == -1) {
                     path.Push(node);
                     return -1;
                 }
@@ -124,18 +130,42 @@ public class Unit : WorldObject {
         }
         return min;
     }
+
+    private List<Vector3> getSuccessors(Vector3 node)
+    {
+        List<Vector3> successors = new List<Vector3>(17);
+        successors.Add(node + (destination - node).normalized*3.0f);
+        successors.Add(node + new Vector3(2.5527f, 0, 2.5527f));
+        successors.Add(node + new Vector3(-2.5527f, 0, -2.5527f));
+        successors.Add(node + new Vector3(2.5527f, 0, -2.5527f));
+        successors.Add(node + new Vector3(-2.5527f, 0, 2.5527f));
+        successors.Add(node + new Vector3(3.0f, 0, 0));
+        successors.Add(node + new Vector3(-3.0f, 0, 0));
+        successors.Add(node + new Vector3(0, 0, 3.0f));
+        successors.Add(node + new Vector3(0, 0, -3.0f));
+        successors.Add(node + new Vector3(2.771f, 0, 1.14805f));
+        successors.Add(node + new Vector3(-2.771f, 0, -1.14805f));
+        successors.Add(node + new Vector3(2.771f, 0, -1.14805f));
+        successors.Add(node + new Vector3(-2.771f, 0, 1.14805f));
+        successors.Add(node + new Vector3(1.14805f, 0, 2.771f));
+        successors.Add(node + new Vector3(-1.14805f, 0, -2.771f));
+        successors.Add(node + new Vector3(1.14805f, 0, -2.771f));
+        successors.Add(node + new Vector3(-1.14805f, 0, 2.771f));
+        return successors;
+    }
     private Vector3 getNextbyAngle(Vector3 pos, float angle)
     {
-        return pos + Quaternion.AngleAxis(angle, Vector3.up) * Vector3.right * .2f;
+        return pos + Quaternion.AngleAxis(angle, Vector3.up) * Vector3.right * 3.0f;
     }
     public bool isGoal(Vector3 pos)
     {
-        return Vector3.Distance(pos, destination)<1.0;
+        return Vector3.Distance(pos, destination) < 3.0f;
     }
 
     public bool isValidPosition(Vector3 pos)
     {
-        Building[] buildings = FindObjectsOfType(typeof(Building)) as Building[];
+        if (pos.x > 80 || pos.x < -80 || pos.z > 80 || pos.x < -80)
+            return false;
         foreach (Building element in buildings)
         {
             if (element.GetSelectionBounds().Contains(pos))
@@ -146,10 +176,15 @@ public class Unit : WorldObject {
 
     private void MakeMove()
     {
-        transform.position = Vector3.MoveTowards(transform.position, pathpoint, Time.deltaTime * moveSpeed);
         if (transform.position == pathpoint)
             pathpoint = path.Pop();
         if (isGoal(transform.position))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * moveSpeed);
+        }
+        else if (pathpoint!=Vector3.zero)
+            transform.position = Vector3.MoveTowards(transform.position, pathpoint, Time.deltaTime * moveSpeed);
+        if (transform.position==destination)
         {
             moving = false;
             movingIntoPosition = false;
