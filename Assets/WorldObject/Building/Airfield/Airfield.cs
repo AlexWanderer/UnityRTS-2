43dbe38cba8b3534a7f61e6076c5c1d873f7ff63
DouglasWiley <wowdoug@gmail.com>
2016-05-03 17:03:26 -0500

    added in filler airfield prefab

---
+++ Assets/WorldObject/Building/Airfield/Airfield.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Airfield : Building
+{
+    protected override void Start()
+    {
+        base.Start();
+        actions = new string[] { "Tank" };
+    }
+
+    public override void PerformAction(string actionToPerform)
+    {
+        base.PerformAction(actionToPerform);
+        CreateUnit(actionToPerform);
+    }
+}
